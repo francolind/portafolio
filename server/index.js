5719e488705628289ae5828777b87adca0975af0
francolind <90268531+francolind@users.noreply.github.com>
2022-09-29 20:12:06 -0300

    app.get

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ app.use(cors());
 
 app.use(express.urlencoded({extended:false}));
 
+app.get('/',(req, res )=> res.send('<h1>hola desde express</h1>'));
+
 app.use(express.json());
 
 app.use(router)
 
-app.get('/',cors,(req, res )=> res.send('<h1>hola desde express</h1>'));
+
 
 app.use((req, res, next)=> {
     res.status(404).json({
